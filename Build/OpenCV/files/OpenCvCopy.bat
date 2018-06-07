e6b034735f0ba3940d026e5b8ed77fa6b286762b
masa0810 <masahiro0810@gmail.com>
2018-06-07 21:03:43 +0900

    Update OpenCvCopy.bat

--- Build/OpenCV/files/OpenCvCopy.bat
+++ Build/OpenCV/files/OpenCvCopy.bat
@@ -1,39 +1,72 @@
 @echo off
+REM ファイルコピー(引数1:ビルドタイプ、引数2:アーキテクチャ、引数3:出力ディレクトリ)
+
 setlocal ENABLEDELAYEDEXPANSION
 
-for %%f in ( opencv_ffmpeg320_64.dll ) do (
-	call :FileCopy %1 %2 %%f
+set version=341
+set debugSuffix=d
+
+set libDir=%~dp0bin\
+set platform=%~2
+
+set srcDir="%libDir%%platform%\"
+
+if /platform==/x64 (
+	call :FileCopy %3 %srcDir% opencv_ffmpeg%version%_64.dll
+) else (
+	call :FileCopy %3 %srcDir% opencv_ffmpeg%version%.dll
 )
+for %%f in ( opencv_img_hash%version% opencv_sfm%version% opencv_world%version% ) do (
+	call :CheckConfig %1 %3 %srcDir% %%f dll %debugSuffix%
+)
+
+set cudaVer=92
+set cudaDir="%~dp0..\..\CUDA\bin\"
+
+for %%f in ( cublas64_%cudaVer%.dll cudart64_%cudaVer%.dll cufft64_%cudaVer%.dll ) do (
+	call :FileCopy %3 %cudaDir% %%f
+)
+
 goto :EOF
 
-rem ファイルコピー
+REM ビルドタイプ判定(引数1:ビルドタイプ、引数2:出力ディレクトリ、引数3:コピー元ディレクトリ、引数4:ファイル名、引数5:拡張子、引数6:サフィックス文字列)
+:CheckConfig
+
+if /%1==/Debug (
+	set suffix=%6
+) else (
+	set suffix=
+)
+call :FileCopy %2 %3 %4%suffix%.%5
+
+exit /b
+goto :EOF
+
+REM ファイルコピー(引数1:出力ディレクトリ、引数2:コピー元ディレクトリ、引数3:ファイル名)
 :FileCopy
 
-if /%1==/x64 (
-	set path=%~dp0bin\x64\
-	set filename=%3
-) else (
-	echo "Win32非対応"
-	goto :EOF
-)
+set dstPath=%~f1
+set srcPath=%~f2
+set filename=%3
 
-set filename1=%~f2%filename%
-set filename2=%path%%filename%
+set filename1=%srcPath%%filename%
+set filename2=%dstPath%%filename%
 
-if exist "%filename2%" (
-	if not exist "%filename1%" (
-		copy "%filename2%" "%~2" /y
-		echo %filename2% Copy
+if exist "%filename1%" (
+	if not exist "%filename2%" (
+		copy "%filename1%" "%dstPath%" /y
+		echo %filename1% Copy
 	) else (
-		for %%a in ( "%filename1%" ) do set oldtime=%%~ta
-		for %%b in ( "%filename2%" ) do set newtime=%%~tb
+		for %%a in ( "%filename2%" ) do set oldtime=%%~ta
+		for %%b in ( "%filename1%" ) do set newtime=%%~tb
 		if "!newtime!" GTR "!oldtime!" (
-			copy "%filename2%" "%~2" /y
-			echo %filename2% Update
+			copy "%filename1%" "%dstPath%" /y
+			echo %filename1% Update
 		)
 	)
 )
 
 exit /b
+goto :EOF
 
 endlocal
