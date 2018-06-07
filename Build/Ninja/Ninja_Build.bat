287fd0cfb0211becadfbb0179a1e51ef3b5eb37b
masa0810 <masahiro0810@gmail.com>
2018-06-07 21:03:13 +0900

    Update Ninja_Build.bat

--- Build/Ninja/Ninja_Build.bat
+++ Build/Ninja/Ninja_Build.bat
@@ -1,32 +1,44 @@
 @echo off
 setlocal
 
-rem Python
-set pythonExe="D:\Anaconda3\python.exe"
+REM Python
+set pythonExe="C:\Library\Python\python.exe"
 
-rem ライブラリパス
-set ninjaDir=ninja-1.7.2
+REM ライブラリパス
+set ninjaDir=ninja
 
-rem 現在のパスの保持
+REM 現在のパスの保持
 for /f "delims=" %%f in ( 'cd' ) do set currentPath=%%f
 
-rem バッチファイルの場所
+REM バッチファイルの場所
 set batchPath=%~dp0
 
-rem ソース置き場
+REM ソース置き場
 cd /d "%batchPath%..\..\"
 for /f "delims=" %%f in ( 'cd' ) do set sourceDir=%%f
 cd /d "%currentPath%"
 
-rem Ninjaパス
+REM Ninjaパス
 set ninjaPath=%sourceDir%\%ninjaDir%
 
-rem ファイルチェック
-if not exist "%ninjaPath%\ninja.exe" (
-	cd /d "%ninjaPath%"
-	echo Create Ninja : %ninjaPath%\ninja.exe
-	%pythonExe% bootstrap.py --platform msvc
-	cd /d "%currentPath%"
+REM Ninjaパス
+set ninjaInstallPath=%sourceDir%\Build\Tools\Ninja
+
+REM ファイルチェック
+if not exist "%ninjaInstallPath%\ninja.exe" (
+	REM ビルド
+	if not exist "%ninjaPath%\ninja.exe" (
+		cd /d "%ninjaPath%"
+		echo Create Ninja : "%ninjaPath%\ninja.exe"
+		%pythonExe% bootstrap.py --platform msvc
+		cd /d "%currentPath%"
+	)
+	REM フォルダ作成
+	if not exist "%ninjaInstallPath%" (
+		md "%ninjaInstallPath%"
+	)
+	REM コピー
+	copy "%ninjaPath%\ninja.exe" "%ninjaInstallPath%"
 )
 
 endlocal
