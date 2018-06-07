4cb63482c08464c5a0f38311c1377bb0996be19e
masa0810 <masahiro0810@gmail.com>
2018-06-07 20:45:56 +0900

    Update CUDA_Build.bat

--- Build/CUDA/CUDA_Build.bat
+++ Build/CUDA/CUDA_Build.bat
@@ -1,72 +1,76 @@
 @echo off
 setlocal
 
-rem FastCopy
-set fastcopyPath=FastCopy330_x64\FastCopy.exe
+REM FastCopy
+set fastcopyPath=FastCopy341_x64\FastCopy.exe
 
-rem FastCopyモード
+REM FastCopyモード
 set fastcopyMode=/force_close
 
-rem ライブラリパス
-set cudnnDir=cudnn-8.0-windows10-x64-v6.0
+REM ビルドバッファ
+set buildBuf=C:\Library\Temp
 
-rem バージョン設定
-set cudnnVersion=6.0
+REM ライブラリパス
+set cudnnDir=cudnn
 
-rem 現在のパスの保持
+REM バージョン設定
+set cudaVersion=9.2patch1
+set cudnnVersion=7.1.4
+
+REM 現在のパスの保持
 for /f "delims=" %%f in ( 'cd' ) do set currentPath=%%f
 
-rem バッチファイルの場所
+REM バッチファイルの場所
 set batchPath=%~dp0
 
-rem ソース置き場
+REM ソース置き場
 cd /d "%batchPath%..\..\"
 for /f "delims=" %%f in ( 'cd' ) do set sourceDir=%%f
 cd /d "%currentPath%"
 
-rem FastCopyパス作成
+REM FastCopyパス作成
 set fastcopyExe="%sourceDir%\Build\Tools\%fastcopyPath%"
-rem FastCopyパス表示
+REM FastCopyパス表示
 echo FastCopy : %fastcopyExe%
 
-rem CUDAパス
-set cudaPath=%CUDA_PATH_V8_0%
-rem CUDAパス表示
+REM CUDAパス
+set cudaPath=%sourceDir%\CUDA
+REM CUDAパス表示
 echo CUDA : %cudaPath%
 
-rem CUDNNパス
+REM CUDAパス確認
+if not exist %cudaPath% (
+	powershell -Command Start-Process -Wait -FilePath cmd.exe -Verb runas -ArgumentList '/c','"mklink /D ""%cudaPath%""" """%CUDA_PATH%"""'
+)
+
+REM CUDNNパス
 set cudnnPath=%sourceDir%\%cudnnDir%
-rem CUDNNパス表示
+REM CUDNNパス表示
 echo CUDNN : %cudnnPath%
 
-rem インストールディレクトリ
-set finalDir=%sourceDir%\Final\CUDA
-
-rem インストールディレクトリ表示
+REM インストールディレクトリ
+set finalDir=%buildBuf%\Final\CUDA
+REM インストールディレクトリ表示
 echo install : %finalDir%
 
-rem インストールディレクトリ削除
+REM インストールディレクトリ削除
 if exist "%finalDir%" (
 	rd /S /Q "%finalDir%"
 )
 
-rem includeディレクトリコピー
-%fastcopyExe% %fastcopyMode% /cmd=diff "%cudaPath%\include" /to="%finalDir%\"
-%fastcopyExe% %fastcopyMode% /cmd=diff "%cudnnPath%\cuda\include" /to="%finalDir%\"
+REM includeディレクトリコピー
+%fastcopyExe% %fastcopyMode% /cmd=diff "%cudaPath%\include" /to="%finalDir%\include"
+%fastcopyExe% %fastcopyMode% /cmd=diff "%cudnnPath%\include" /to="%finalDir%\include"
 
-rem libファイルコピー
+REM libファイルコピー
 %fastcopyExe% %fastcopyMode% /cmd=diff /include="*.lib" "%cudaPath%\lib\x64" /to="%finalDir%\lib\x64"
-%fastcopyExe% %fastcopyMode% /cmd=diff /include="*.lib" "%cudnnPath%\cuda\lib\x64" /to="%finalDir%\lib\x64"
+%fastcopyExe% %fastcopyMode% /cmd=diff /include="*.lib" "%cudnnPath%\lib\x64" /to="%finalDir%\lib\x64"
 
-rem dll・pdbファイルコピー
+REM dll・pdbファイルコピー
 %fastcopyExe% %fastcopyMode% /cmd=diff /include="*.dll" /exclude="*32*" "%cudaPath%\bin" /to="%finalDir%\bin"
-%fastcopyExe% %fastcopyMode% /cmd=diff /include="*.dll" "%cudnnPath%\cuda\bin" /to="%finalDir%\bin"
+%fastcopyExe% %fastcopyMode% /cmd=diff /include="*.dll" "%cudnnPath%\bin" /to="%finalDir%\bin"
 
-rem Copyバッチのコピー
-%fastcopyExe% %fastcopyMode% /cmd=diff "%batchPath%\files\CudaCopy.bat" /to="%finalDir%\"
-
-rem バージョン番号ファイル追加
-for /f "tokens=3" %%l in ( 'type "%cudaPath%\version.txt"' ) do set cudaVersion=%%l
+REM バージョン番号ファイル追加
 type nul > %finalDir%\CUDA_%cudaVersion%
 type nul > %finalDir%\cuDNN_%cudnnVersion%
 
