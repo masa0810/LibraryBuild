b4f1147e55fcaf44659c11bf58668f410f9de4ff
masa0810 <masahiro0810@gmail.com>
2018-06-07 20:48:15 +0900

    Update Eigen_Build.bat

--- Build/Eigen/Eigen_Build.bat
+++ Build/Eigen/Eigen_Build.bat
@@ -1,57 +1,128 @@
 @echo off
 setlocal
 
-rem FastCopy
-set fastcopyPath=FastCopy330_x64\FastCopy.exe
+REM CMake
+set cmakePath=CMake\bin\cmake.exe
 
-rem FastCopyモード
+REM ninja
+set ninjaPath=Ninja\ninja.exe
+
+REM FastCopy
+set fastcopyPath=FastCopy341_x64\FastCopy.exe
+
+REM FastCopyモード
 set fastcopyMode=/force_close
 
-rem ライブラリパス
-set eigenDir=eigen-eigen-5a0156e40feb
+REM ビルドバッファ
+set buildBuf=C:\Library\Temp
 
-rem バージョン設定
+REM ライブラリパス
+set eigenDir=Eigen
+
+REM バージョン設定
 set eigenVersion=3.3.4
 
-rem 現在のパスの保持
+REM 現在のパスの保持
 for /f "delims=" %%f in ( 'cd' ) do set currentPath=%%f
 
-rem バッチファイルの場所
+REM バッチファイルの場所
 set batchPath=%~dp0
 
-rem ソース置き場
+REM ソース置き場
 cd /d "%batchPath%..\..\"
 for /f "delims=" %%f in ( 'cd' ) do set sourceDir=%%f
 cd /d "%currentPath%"
 
-rem FastCopyパス作成
+REM CMakeパス作成
+set cmakeExe="%sourceDir%\Build\Tools\%cmakePath%"
+REM CMakeパス表示
+echo CMake : %cmakeExe%
+
+REM Ninja
+set ninjaExe="%sourceDir%\Build\Tools\%ninjaPath%"
+REM Ninjaパス表示
+echo Ninja : %ninjaExe%
+
+REM Ninjaファイルチェック
+call "%sourceDir%\Build\Ninja\Ninja_Build.bat"
+
+REM FastCopyパス作成
 set fastcopyExe="%sourceDir%\Build\Tools\%fastcopyPath%"
-rem FastCopyパス表示
+REM FastCopyパス表示
 echo FastCopy : %fastcopyExe%
 
-rem Eigenパス
+REM Eigenパス
 set eigenPath=%sourceDir%\%eigenDir%
-rem Eigenパス表示
+REM Eigenパス表示
 echo Eigen : %eigenPath%
 
-rem インストールディレクトリ
-set finalDir=%sourceDir%\Final\Eigen
+REM アドレスモデル切り替え
+if /%Platform%==/ (
+	set platformName=Win32
+) else (
+	set platformName=x64
+)
 
-rem インストールディレクトリ表示
+REM Visual Studioバージョン切り替え
+if /%VisualStudioVersion%==/11.0 (
+	set vsVersion=vs2012
+) else if /%VisualStudioVersion%==/12.0 (
+	set vsVersion=vs2013
+) else if /%VisualStudioVersion%==/14.0 (
+	set vsVersion=vs2015
+) else if /%VisualStudioVersion%==/15.0 (
+	set vsVersion=vs2017
+) else (
+	set vsVersion=vs2010
+)
+
+REM ビルドディレクトリ
+set buildDir=%buildBuf%\%vsVersion%\%platformName%\Eigen
+REM ビルドディレクトリ表示
+echo build directory : %buildDir%
+REM ビルドディレクトリ確認
+if not exist "%buildDir%" (
+	mkdir "%buildDir%"
+)
+REM ビルドディレクトリへ移動
+cd /d "%buildDir%"
+
+%cmakeExe% "%eigenPath%" ^
+-G "Ninja" ^
+-DCMAKE_MAKE_PROGRAM=%ninjaExe:\=/% ^
+-DCMAKE_BUILD_TYPE="Release" ^
+-DCMAKE_INSTALL_PREFIX="%buildDir:\=/%/install"
+
+%ninjaExe% install
+call :ErrorCheck
+
+REM インストールディレクトリ
+set finalDir=%buildBuf%\Final\Eigen
+REM インストールディレクトリ表示
 echo install : %finalDir%
 
-rem インストールディレクトリ削除
+REM インストールディレクトリ削除
 if exist "%finalDir%" (
 	rd /S /Q "%finalDir%"
 )
 
-rem Eigenディレクトリコピー
-%fastcopyExe% %fastcopyMode% /cmd=diff /exclude="*.txt;*.md" "%eigenPath%\Eigen" /to="%finalDir%\include\"
+REM Eigenディレクトリコピー
+%fastcopyExe% %fastcopyMode% /cmd=diff /exclude="*.txt;*.md" "install\include\eigen3" /to="%finalDir%\include"
 
-rem Eigenディレクトリコピー
-%fastcopyExe% %fastcopyMode% /cmd=diff /exclude="*.txt;*.md" "%eigenPath%\unsupported\Eigen" /to="%finalDir%\include\unsupported\"
-
-rem バージョン番号ファイル追加
+REM バージョン番号ファイル追加
 type nul > %finalDir%\Eigen_%eigenVersion%
+
+goto :EOF
+
+:ErrorCheck
+if not %errorlevel% == 0 (
+	echo ERROR
+	cd /d "%currentPath%"
+	exit 1
+) else (
+	exit /b
+)
+
+goto :EOF
 
 endlocal
