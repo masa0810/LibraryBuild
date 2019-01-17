988ad8447f78bd6709b2c15c6e51f555fe9c0dc1
masa0810 <masahiro0810@gmail.com>
2019-01-17 21:19:38 +0900

    Update CUDA.py

--- Build/CUDA/CUDA.py
+++ Build/CUDA/CUDA.py
@@ -5,6 +5,7 @@ CUDA Build
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 import sys
 from pathlib import Path
 
@@ -18,51 +19,37 @@ def cuda_copy(final_dir, lib_dir, platform_name, enable_shared, enable_debug,
     # シンボリックリンク作成
     if not lib_dir.exists():
         cuda_path = os.getenv(
-            "CUDA_PATH_V9_2",
-            r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.2")
-        subprocess.check_call(
-            ["mklink", "/d", str(lib_dir), cuda_path], shell=True)
+            "CUDA_PATH_V10_0",
+            r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0")
+        subprocess.check_call([
+            "powershell", "-Command", "Start-Process", "-Wait", "-FilePath",
+            "cmd", "-Verb", "runas", "-ArgumentList",
+            '/c, "mklink /D ""{:s}"" ""{:s}"" "'.format(
+                str(lib_dir), cuda_path)
+        ])
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        str(lib_dir / "include"), "/to={:s}".format(
-            str(final_dir / "include"))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    # include
+    result_state = com.copy_command(
+        args, str(lib_dir / "include"), "/to={:s}".format(
+            str(final_dir / "include")))
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        "/include=*.dll", "/exclude=*32*",
-        str(lib_dir / "bin"), "/to={:s}".format(
-            str(final_dir / "bin" / platform_name))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    # bin
+    result_state &= com.copy_command(args, "/include=*.dll", "/exclude=*32*",
+                                     str(lib_dir / "bin"), "/to={:s}".format(
+                                         str(final_dir / "bin")))
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        "/include=*.lib",
-        str(lib_dir / "lib" / platform_name), "/to={:s}".format(
-            str(final_dir / "lib" / platform_name))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    # lib
+    result_state &= com.copy_command(
+        args, "/include=*.lib", str(lib_dir / "lib" / platform_name),
+        "/to={:s}".format(str(final_dir / "lib" / platform_name)))
+
+    # 依存DLL
+    driver_path = Path(__file__).resolve().parent / "NvidiaDriver"
+    result_state &= com.copy_command(
+        args, str(driver_path / "nvcuda.dll"),
+        str(driver_path / "nvcuvid.dll"),
+        str(driver_path / "nvfatbinaryLoader.dll"), "/to={:s}".format(
+            str(final_dir / "bin")))
 
     return result_state
 
@@ -75,7 +62,7 @@ if __name__ == "__main__":
     # ライブラリ名
     cuda_name = "CUDA"
     # バージョン設定
-    cuda_ver = "9.2patch1"
+    cuda_ver = "10.0.130"
     # ビルド実行
     success = com.build(
         cuda_name,
@@ -89,7 +76,7 @@ if __name__ == "__main__":
     # ライブラリ名
     cudnn_name = "cuDNN"
     # バージョン設定
-    cudnn_ver = "7.2.1"
+    cudnn_ver = "7.4.2.24"
     # ビルド実行
     success = com.build(
         cudnn_name,
