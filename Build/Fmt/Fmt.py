7928b683b89cf55ec17ad436852fa4db4f92c00d
masa0810 <masahiro0810@gmail.com>
2019-01-17 21:20:36 +0900

    Update Fmt.py

--- Build/Fmt/Fmt.py
+++ Build/Fmt/Fmt.py
@@ -14,39 +14,31 @@ import Common as com
 def result_copy(final_dir, build_dir, platform_name, enable_shared,
                 enable_debug, args):
     """ビルド結果コピー処理"""
-    # FastCopyの引数作成
+    # include
     include_dir = final_dir / "include"
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        str(build_dir / "install" / "include"), "/to={:s}".format(
-            str(include_dir))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    result_state = com.copy_command(args, str(
+        build_dir / "install" / "include"), "/to={:s}".format(
+            str(include_dir)))
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        str(Path(__file__).resolve().parent / "files" / "fmtset.h"),
-        "/to={:s}".format(str(include_dir))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    # セットヘッダ
+    result_state &= com.copy_command(
+        args, str(Path(__file__).resolve().parent / "files" / "fmtset.h"),
+        "/to={:s}".format(str(include_dir)))
 
     return result_state
 
 
-def create_cmake_args(cmake_args, source_path, platform_name, vc_ver, args,
-                      enable_shared, enable_debug):
+def create_cmake_args(cmake_args, source_path, platform_name, vs_ver, vc_ver,
+                      args, enable_shared, enable_debug):
     """CMakeの引数作成"""
+    arch_flag = ("/arch:AVX2"
+                 if args.enable_avx2 else "/arch:AVX") if vc_ver == 141 else ""
+    cmake_args.append(
+        "-DCMAKE_CXX_FLAGS=/DWIN32 /D_WINDOWS /W0 /GR /EHsc /bigobj {:s}".
+        format(arch_flag))
+    cmake_args.append("-DCMAKE_C_FLAGS=/DWIN32 /D_WINDOWS /W0 /bigobj {:s}".
+                      format(arch_flag))
+
     cmake_args.append("-DFMT_DOC=OFF")
     cmake_args.append("-DFMT_TEST=OFF")
 
@@ -61,7 +53,7 @@ if __name__ == "__main__":
     # ライブラリ名
     lib_name = "Fmt"
     # バージョン設定
-    lib_ver = "5.2.1"
+    lib_ver = "5.3.0"
 
     # ビルド実行
     success = com.build(
