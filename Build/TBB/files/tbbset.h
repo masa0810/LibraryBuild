2eadc5f19bd944aac7f0edd0131c41058772e5f3
masa0810 <masahiro0810@gmail.com>
2019-01-17 21:25:02 +0900

    Update tbbset.h

--- Build/TBB/files/tbbset.h
+++ Build/TBB/files/tbbset.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #ifndef TBB_PREVIEW_VARIADIC_PARALLEL_INVOKE
     #define TBB_PREVIEW_VARIADIC_PARALLEL_INVOKE 1
@@ -15,18 +15,18 @@
 #ifndef _LIB
     #ifndef __TBB_NO_IMPLICIT_LINKAGE
         #define __TBB_NO_IMPLICIT_LINKAGE 1
-
+        
         // サフィックス設定
         #ifdef TBB_USE_DEBUG
             #define __TBBSET_PREFIX__ "_debug"
         #else
             #define __TBBSET_PREFIX__
         #endif
-
+        
         // リンク指定
         #pragma comment(lib, "tbb" __TBBSET_PREFIX__ ".lib")
         #pragma comment(lib, "tbbmalloc" __TBBSET_PREFIX__ ".lib")
-
+        
         // アロケータ入れ替え
         #ifdef TBB_ENABLE_PROXY
             #pragma comment(lib, "tbbmalloc_proxy" __TBBSET_PREFIX__ ".lib")
