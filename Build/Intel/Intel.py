149e0a7c30363d337cd2c3dd0718bc62e7c3e863
masa0810 <masahiro0810@gmail.com>
2019-01-17 21:21:42 +0900

    Update Intel.py

--- Build/Intel/Intel.py
+++ Build/Intel/Intel.py
@@ -11,8 +11,8 @@ sys.path.append(str((Path(__file__).parent.parent / "Common").resolve()))
 import Common as com
 
 
-def intel_copy(final_dir, lib_dir, platform_name, enable_shared, enable_debug,
-               args):
+def lib_copy(final_dir, lib_dir, platform_name, enable_shared, enable_debug,
+             args):
     """ビルド結果コピー処理"""
     intel_lib_dir = Path(
         r"C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows")
@@ -23,46 +23,22 @@ def intel_copy(final_dir, lib_dir, platform_name, enable_shared, enable_debug,
     # プラットフォーム名
     platform_intel = "intel64" if platform_name == "x64" else "ia32"
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        str(intel_lib_dir / lib_name_intel / "include"), "/to={:s}".format(
-            str(final_dir / "include"))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+    # include
+    result_state = com.copy_command(
+        args, str(intel_lib_dir / lib_name_intel / "include"),
+        "/to={:s}".format(str(final_dir / "include")))
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        "/include=*.dll", "/exclude=*32*",
+    # bin
+    result_state &= com.copy_command(
+        args, "/include=*.dll", "/exclude=*32*",
         str(intel_lib_dir / "redist" / platform_intel / lib_name_intel / "*"),
-        "/to={:s}".format(str(final_dir / "bin" / platform_name))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+        "/to={:s}".format(str(final_dir / "bin" / platform_name)))
 
-    # FastCopyの引数作成
-    fastcopy_args = [
-        str(args.fastcopy_path), args.fastcopy_mode,
-        "/include=*.lib",
+    # lib
+    result_state &= com.copy_command(
+        args, "/include=*.lib",
         str(intel_lib_dir / lib_name_intel / "lib" / platform_intel / "*"),
-        "/to={:s}".format(str(final_dir / "lib" / platform_name))
-    ]
-    if args.verbose:
-        print("FastCopy Args :")
-        for i in fastcopy_args:
-            print(i)
-    # FastCopyを実行
-    result_state = com.run_proc(fastcopy_args)
+        "/to={:s}".format(str(final_dir / "lib" / platform_name)))
 
     return result_state
 
@@ -72,18 +48,19 @@ if __name__ == "__main__":
     # 引数取得
     args = com.get_args()
 
+    # バージョン設定
+    intel_ver = "2019Update1"
+
     # ライブラリ名
     mkl_name = "MKL"
-    # バージョン設定
-    intel_ver = "2018Update3"
     # ビルド実行
     success = com.build(
-        mkl_name, intel_ver, args, None, intel_copy, enable_vs_share=True)
+        mkl_name, intel_ver, args, None, lib_copy, enable_vs_share=True)
 
     # ライブラリ名
     ipp_name = "IPP"
     # ビルド実行
     success = com.build(
-        ipp_name, intel_ver, args, None, intel_copy, enable_vs_share=True)
+        ipp_name, intel_ver, args, None, lib_copy, enable_vs_share=True)
 
     sys.exit(0 if success else -1)
